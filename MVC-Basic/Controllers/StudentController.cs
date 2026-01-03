17c63a4ef536ff659b6ae28087df2f4db4d8a129
Vinayak-patil27 <vinupatil7707@gmail.com>
2026-01-03 08:02:08 +0530

    Create Basic Student Registration form Only Post method and push it into List<StudentRegistrationViewModel>

---
+++ MVC-Basic/Controllers/StudentController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Basic.ViewModel;
+
+namespace MVC_Basic.Controllers
+{
+    public class StudentController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(StudentRegistrationViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            // Save to DB
+
+            return View("Success", model); // stay on same page
+        }
+
+        public IActionResult Success()
+        {
+            return Content("Registration successful!");
+        }
+
+    }
+}
---
+++ MVC-Basic/ViewModel/StudentRegistrationViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace MVC_Basic.ViewModel
+{
+    public class StudentRegistrationViewModel
+    {
+        // Personal Details
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string Gender { get; set; }
+
+        // Contact Information
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+
+        // Academic Details
+        public string StudentId { get; set; }
+        public string Course { get; set; }
+    }
+
+
+}
