1c770eb2ae9bee4d54840bb45049923008435a17
Vinayak-patil27 <vinupatil7707@gmail.com>
2026-01-06 22:22:45 +0530

    Add BankMaster in Master Area and done Insert and View in List<Bank>

---
+++ MVC-Basic/Areas/Master/Controllers/BankController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Basic.Areas.Master.Models;
+using System.Reflection;
+
+namespace MVC_Basic.Areas.Master.Controllers
+{
+
+	[Area("Master")]
+	public class BankController : Controller
+    {
+        private static List<Bank> banks = new List<Bank>();
+        public IActionResult Index()
+        {
+            return View(banks);
+        }
+
+        public IActionResult Insert(Bank bank)
+        {
+			if (!ModelState.IsValid)
+				return View(bank);
+            banks.Add(new Bank
+            {
+                BankId = banks.Count+1,
+                Name = bank.Name,
+                IFSCCode = bank.IFSCCode,
+                Address = bank.Address,
+                MICRCode = bank.MICRCode
+            });
+            return RedirectToAction("Index");
+		}
+    }
+}
---
+++ MVC-Basic/Areas/Master/Models/Bank.cs
@@ -0,0 +1,11 @@
+﻿namespace MVC_Basic.Areas.Master.Models
+{
+    public class Bank
+    {
+        public int BankId { get; set; }
+        public string Name { get; set; }
+        public string IFSCCode { get; set; }
+        public string Address { get; set; }
+        public string MICRCode { get; set; }
+    }
+}
