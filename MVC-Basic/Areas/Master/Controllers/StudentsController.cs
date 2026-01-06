a5e44d2f7719486d12d8ad374663b023c720f18d
Vinayak-patil27 <vinupatil7707@gmail.com>
2026-01-06 08:34:00 +0530

    Learn About Arera (ie. We Can Create Seprate Modules Like Master,Tansaction and each area has its own controller,view model) and Route area in program.cs

---
+++ MVC-Basic/Areas/Master/Controllers/StudentsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Basic.Areas.Master.Controllers
+{
+    [Area("Master")]
+    public class StudentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
