a769b3335515e776dc35db016a2a541708d8d2cb
Vinayak-patil27 <vinupatil7707@gmail.com>
2026-01-07 08:28:31 +0530

    Use taghepler like asp-action asp-for and implement edit and delete in master area for Bank Master (CURD complete)

--- MVC-Basic/Areas/Master/Controllers/BankController.cs
+++ MVC-Basic/Areas/Master/Controllers/BankController.cs
@@ -5,28 +5,81 @@ using System.Reflection;
 namespace MVC_Basic.Areas.Master.Controllers
 {
 
-	[Area("Master")]
-	public class BankController : Controller
+    [Area("Master")]
+    public class BankController : Controller
     {
         private static List<Bank> banks = new List<Bank>();
         public IActionResult Index()
         {
             return View(banks);
         }
+        //public IActionResult Insert()
+        //{
+        //    return View(); 
+        //}
+
 
         public IActionResult Insert(Bank bank)
         {
-			if (!ModelState.IsValid)
-				return View(bank);
+            if (!ModelState.IsValid)
+                return View(bank);
             banks.Add(new Bank
             {
-                BankId = banks.Count+1,
+                BankId = banks.Count + 1,
                 Name = bank.Name,
                 IFSCCode = bank.IFSCCode,
                 Address = bank.Address,
                 MICRCode = bank.MICRCode
             });
             return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            try
+            {
+                var bank = banks.Where(x => x.BankId == id).FirstOrDefault();
+                return View(bank);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Bank bank)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    banks.RemoveAt(bank.BankId);
+                    banks.Add(bank);
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View(bank);
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Delete(int id)
+		{
+			var bank = banks.FirstOrDefault(x => x.BankId == id);
+			if (bank == null)
+				return NotFound();
+
+			banks.Remove(bank);
+			return RedirectToAction("Index");
 		}
-    }
+
+	}
 }
