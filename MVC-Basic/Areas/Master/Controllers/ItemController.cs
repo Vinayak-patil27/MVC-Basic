c44609abe7584bc3683ace6bd1ca0be35a1e4632
Vinayak-patil27 <vinupatil7707@gmail.com>
2026-01-09 05:51:51 +0530

    add Item Master form with CURD Operations

---
+++ MVC-Basic/Areas/Master/Controllers/ItemController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Basic.Areas.Master.Models;
+using System.Diagnostics.Contracts;
+using System.Reflection;
+
+namespace MVC_Basic.Areas.Master.Controllers
+{
+    [Area("Master")]
+    public class ItemController : Controller
+    {
+        private static List<Item> itemlist = new();
+        public IActionResult Index()
+        {
+            return View(itemlist);
+        }
+
+        public IActionResult ItemForm(int? id)
+        {
+            try
+            {
+				// if id=0 then Insert
+				if (!id.HasValue || id <= 0)
+				{
+					return View(new Item());
+				}
+
+				//if id>0 then edit
+				var record = itemlist.FirstOrDefault(x => x.ItemCode == id);
+				if (record == null)
+				{
+					return NotFound();
+				}
+				return View(record);
+			}
+            catch(Exception ex)
+            {
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult ItemForm(Item Model)
+        {
+            try
+            {
+				if (!ModelState.IsValid)
+				{
+					return View(Model);
+				}
+
+				if (Model.ItemCode == 0)
+				{
+					Model.ItemCode = itemlist.Count + 1;
+					itemlist.Add(Model);
+				}
+				else
+				{
+					var existing = itemlist.FirstOrDefault(x => x.ItemCode == Model.ItemCode);
+					if (existing == null)
+						return NotFound();
+
+					existing.ItemName = Model.ItemName;
+					existing.ItemGroupCode = Model.ItemGroupCode;
+					existing.ItemCompanyCode = Model.ItemCompanyCode;
+					existing.UnitCode = Model.UnitCode;
+					existing.MRP = Model.MRP;
+					existing.Rate = Model.Rate;
+					existing.Barcode = Model.Barcode;
+					existing.TaxID = Model.TaxID;
+					existing.Description = Model.Description;
+				}
+				return RedirectToAction("Index");
+			}
+            catch (Exception ex)
+            {
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                var record = itemlist.FirstOrDefault(x => x.ItemCode == id);
+                if (record == null)
+                    return NotFound();
+
+                itemlist.Remove(record);
+                return RedirectToAction("Index");
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ MVC-Basic/Areas/Master/Models/Item.cs
@@ -0,0 +1,18 @@
+﻿namespace MVC_Basic.Areas.Master.Models
+{
+    public class Item
+    {
+        public int ItemCode { get; set; }
+        public string ItemName { get; set; }
+        public int ItemGroupCode { get; set; }
+        public int ItemCompanyCode { get; set; }
+        public int UnitCode { get; set; }
+        public string Barcode { get; set; }
+        public double MRP { get; set; }
+        public double Rate { get; set; }
+        public int HSCCode { get; set; }
+        public int TaxID { get; set; }
+        public string Description { get; set; }
+        
+    }
+}
