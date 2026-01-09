657a5b009786cd3bd60b36c74d2d25fb9613cdf0
Vinayak-patil27 <vinupatil7707@gmail.com>
2026-01-09 08:13:10 +0530

    Unit Master with curd operations

---
+++ MVC-Basic/Areas/Master/Controllers/UnitController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Basic.Areas.Master.Models;
+using System.Reflection.Metadata.Ecma335;
+
+namespace MVC_Basic.Areas.Master.Controllers
+{
+	[Area("Master")]
+	public class UnitController : Controller
+	{
+		private static List<Unit> unitlist = new();
+		public IActionResult Index()
+		{
+			return View(unitlist);
+		}
+
+		[HttpGet]
+		public IActionResult UnitForm(int? id)
+		{
+			try
+			{
+				if (id == null || id == 0)
+				{
+					return View(new Unit());
+				}
+				else
+				{
+					var rec = unitlist.FirstOrDefault(x => x.UnitId == id);
+					if (rec == null)
+					{
+						return NotFound();
+					}
+					return View(rec);
+				}
+			}
+			catch (Exception ex)
+			{
+				throw ex;
+			}
+		}
+
+		[HttpPost]
+		public IActionResult UnitForm(Unit unit)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View(unit);
+			}
+				if (unit.UnitId == 0)
+				{
+					unit.UnitId = unitlist.Count + 1;
+					unitlist.Add(unit);
+				}
+				else
+				{
+					var rec = unitlist.FirstOrDefault(x => x.UnitId == unit.UnitId);
+					if (rec == null)
+						return NotFound();
+					rec.UnitName = unit.UnitName;
+					rec.IsSubUnit = unit.IsSubUnit;
+					rec.SubUnitId = unit.SubUnitId;
+				}
+			return RedirectToAction("Index");
+		}
+
+
+		[HttpPost]
+		public IActionResult Delete(int id)
+		{
+			try
+			{
+				var record = unitlist.FirstOrDefault(x => x.UnitId == id);
+				if (record == null)
+					return NotFound();
+
+				unitlist.Remove(record);
+				return RedirectToAction("Index");
+			}
+			catch(Exception ex)
+			{
+				throw ex;
+			}
+		}
+
+	}
+}
---
+++ MVC-Basic/Areas/Master/Models/Unit.cs
@@ -0,0 +1,10 @@
+﻿namespace MVC_Basic.Areas.Master.Models
+{
+	public class Unit
+	{
+		public int UnitId { get; set; }
+		public string UnitName { get; set; }
+		public int IsSubUnit { get; set; }
+		public int SubUnitId { get; set; }		
+	}
+}
