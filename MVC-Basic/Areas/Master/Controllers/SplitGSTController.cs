5e1d612485b2025bdeeaefa9010dde220baf27cb
Vinayak-patil27 <vinupatil7707@gmail.com>
2026-01-12 08:25:41 +0530

    Add SptilGST

---
+++ MVC-Basic/Areas/Master/Controllers/SplitGSTController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Basic.Areas.Master.Models;
+
+namespace MVC_Basic.Areas.Master.Controllers
+{
+	[Area("Master")]
+	public class SplitGSTController : Controller
+	{
+		private static List<SplitGST> gstlist = new();
+		public IActionResult Index()
+		{
+			return View(gstlist);
+		}
+
+		[HttpGet]
+		public IActionResult SplitGstForm(int? id)
+		{
+			try
+			{
+				if (id == null || id == 0)
+				{
+					return View(new SplitGST());
+				}
+				else
+				{
+					var rec = gstlist.FirstOrDefault(x => x.GSTId == id);
+					if (rec != null)
+					{
+						return NotFound();
+					}
+					return View(rec);
+				}
+			}
+			catch(Exception ex)
+			{
+				throw ex;
+			}
+		}
+
+		[HttpPost]
+		public IActionResult SplitGstForm(SplitGST splitgst) 
+		{
+			try
+			{
+				if(!ModelState.IsValid)
+				{
+					return View();
+				}
+				if (splitgst.GSTId == 0)
+				{
+					splitgst.GSTId = gstlist.Count + 1;
+					gstlist.Add(splitgst);
+				}
+				else
+				{
+					var rec = gstlist.FirstOrDefault(x => x.GSTId == splitgst.GSTId);
+					if (rec != null)
+					{
+						return NotFound();
+					}
+					rec.TaxId = splitgst.TaxId;
+					rec.cgst = splitgst.cgst;
+					rec.sgst = splitgst.sgst;
+					rec.igst = splitgst.igst;
+					rec.ValideDate = splitgst.ValideDate;
+					gstlist.Add(rec);
+				}
+				return RedirectToAction("Index");
+			}
+			catch(Exception ex)
+			{
+				throw ex;
+			}
+		}
+
+		[HttpPost]
+		public IActionResult Delete(int id)
+		{
+			try
+			{
+				var rec = gstlist.FirstOrDefault(x => x.GSTId == id);
+				if(rec==null)
+				{
+					return NotFound();
+				}
+				gstlist.Remove(rec);
+				return RedirectToAction("Index");
+			}
+			catch(Exception ex)
+			{
+				throw ex;
+			}
+		}
+	}
+}
---
+++ MVC-Basic/Areas/Master/Models/SplitGST.cs
@@ -0,0 +1,12 @@
+﻿namespace MVC_Basic.Areas.Master.Models
+{
+	public class SplitGST
+	{
+		public int GSTId { get; set; }
+		public int TaxId { get; set; }
+		public DateTime ValideDate { get; set; }
+		public double? cgst { get; set; }
+		public double? sgst { get; set; }
+		public double? igst { get; set; }
+	}
+}
